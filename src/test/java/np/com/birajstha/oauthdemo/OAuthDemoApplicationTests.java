9f7acd02c9f0db9b50fafcdc1fc5161521ac5002
Biraj Shrestha <birajdotdeb@gmail.com>
2026-09-17 08:10:33 +0545

    Initial commit

---
+++ src/test/java/np/com/birajstha/oauthdemo/OAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.birajstha.oauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
